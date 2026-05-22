7c7944c1ca8be41766a593b3b5e2f93b8a700539
Mclarenlife <mclarenlife@qq.com>
2026-05-22 20:24:43 +0800

    Initial Commit

---
+++ MINIGT_spaceTests/MINIGT_spaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  MINIGT_spaceTests.swift
+//  MINIGT_spaceTests
+//
+//  Created by Mclarenlife on 2026/5/22.
+//
+
+import Testing
+@testable import MINIGT_space
+
+struct MINIGT_spaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
